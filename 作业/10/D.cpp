2c5a5a046267ef59bf7f58d6220991581b0e8fb6
cliche <745356675@qq.com>
2021-05-10 15:14:36 +0800

    -

--- 作业/10/D.cpp
+++ 作业/10/D.cpp
@@ -3,14 +3,13 @@
 #define ll long long
 using namespace std;
 
-const int inf = 1e6 + 2;
-int n = 0;
+const int inf = 1e6 + 10;
 pair<int, int> nums[inf];
 int sums[inf] = {0};
 int cnt[inf] = {0};
 
 #define lowbit(x) (x & (-x))
-int ask(int x) {
+int query(int x) {
     int res = 0;
     for (int i = x; i >= 1; i -= lowbit(i))
         res += sums[i];
@@ -18,7 +17,7 @@ int ask(int x) {
 }
 
 void update(int x) {
-    for (int i = x; i <= n; i += lowbit(i))
+    for (int i = x; i < inf; i += lowbit(i))
         sums[i]++;
 }
 
@@ -26,20 +25,22 @@ int main() {
     ios::sync_with_stdio(false);
     freopen("作业/data/a.in", "r", stdin);
     freopen("作业/data/a.out", "w", stdout);
-
+    int n = 0;
     cin >> n;
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) {
         cin >> nums[i].first >> nums[i].second;
+        nums[i].second++;
+    }
     
     sort(nums + 1, nums + n + 1);
     
     for (int i = 1; i <= n; i++) {
-        cnt[ask(nums[i].second + 1)]++;
-        update(nums[i].second + 1);
+        cnt[query(nums[i].second)]++;
+        update(nums[i].second);
     }
 
     for (int i = 0; i < n; i++)
-        printf("%d\n", cnt[i]);
+        cout << cnt[i] << '\n';
 
     return 0;
 }
--- 作业/10/E.cpp
+++ 作业/10/E.cpp
@@ -1,60 +1,57 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
 const int inf = 2e5 + 1;
-int n = 0;
-int a = 0;
+int tree[inf << 2];
 int p = 0;
-int d[inf << 2];
-int s[inf];
+int a = 0;
 
-void build(int x, int l, int r) {
-    if (l >= r) {
-        d[x] = s[l];
+void update(int root, int left, int right, int pos, int newVal) {
+    if (left == right) {
+        tree[root] = newVal;
         return;
     }
-    int m = (l + r) / 2;
-    build(x * 2, l, m);
-    build(x * 2 + 1, m + 1, r);
-    d[x] = max(d[x * 2], d[x * 2 + 1]);
+    int mid = (left + right) / 2;
+    if (pos <= mid)
+        update(root << 1, left, mid, pos, newVal);
+    else
+        update(root << 1 | 1, mid + 1, right, pos, newVal);
+    tree[root] = max(tree[root << 1], tree[root << 1 | 1]);
 }
 
-int ask(int x, int l, int r, int p1, int p2) {
-    if (l == p1 && r == p2)
-        return d[x];
-    int m = (l + r) / 2;
-    if (p2 <= m)
-        return ask(x * 2, l, m, p1, p2);
-    if (p1 > m)
-        return ask(x * 2 + 1, m + 1, r, p1, p2);
-    int lch_val = ask(x * 2, l, m, p1, m);
-    int rch_val = ask(x * 2 + 1, m + 1, r, m + 1, p2);
-    return max(lch_val, rch_val);
+int query(int root, int left, int right, int lower_bound, int upper_bound) {
+    if (left == lower_bound && right == upper_bound)
+        return tree[root];
+    int mid = (left + right) / 2;
+    if (upper_bound <= mid)
+        return query(root << 1, left, mid, lower_bound, upper_bound);
+    if (lower_bound > mid)
+        return query(root << 1 | 1, mid + 1, right, lower_bound, upper_bound);
+    // 分散在两个区间
+    return max(query(root << 1, left, mid, lower_bound, mid), query(root << 1 | 1, mid + 1, right, mid + 1, upper_bound));
 }
-
 
 int main() {
-    ios::sync_with_stdio(false);
+    freopen("作业/data/a.in", "r", stdin);
+    freopen("作业/data/a.out", "w", stdout);
     int m;
+    int n = 0;
     cin >> m >> p;
-
     for (int i = 0; i < m; i++) {
         char opt;
         cin >> opt;
         if (opt == 'Q') {
             int l = 0;
             cin >> l;
-            a = ask(1, 1, n, n - l + 1, n);
-            cout << a << endl;
+            a = query(1, 1, 2e5, n - l + 1, n);
+            cout << a << '\n';
         } else {
             int t = 0;
             cin >> t;
-            s[++n] = (t + a) % p;
-            build(1, 1, n);
+            update(1, 1, 2e5, ++n, (t + a) % p);
         }
     }
-    
+
     return 0;
 }
