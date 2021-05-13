d3c0704a214373901ab643e2c0a267cd640851b6
cliche <745356675@qq.com>
2021-05-13 13:36:41 +0800

    -

--- 作业/11/A.cpp
+++ 作业/11/A.cpp
@@ -5,31 +5,37 @@ using namespace std;
 const int maxn = 1e6 + 1;
 const int mod = 998244353;
 int dp[maxn];
-int number[maxn];
+int sums[maxn];
 bool illegal[maxn] = {false};
 
 int main() {
     freopen("作业/data/a.in", "r", stdin);
     freopen("作业/data/a.out", "w", stdout);
 
-    int n, m, k;
+    int n, m, k, t, times;
     cin >> n >> m >> k;
     
     for (int i = 0; i < m; i++) {
-        cin >> number[i];
-        illegal[number[i]] = true;
+        cin >> t;
+        illegal[t] = true;
     }
     
-    dp[0] = 1;
+    sums[0] = dp[0] = 1;
     for (int i = 1; i <= n; i++) {
         if (illegal[i]) {
             dp[i] = 0;
+            sums[i] = sums[i - 1];
             continue;
         }
-        for (int j = 1; j <= k && i - j >= 0; j++)
-            dp[i] += dp[i - j];
-        dp[i] %= mod;
-    }   
+        if (i <= k)
+            dp[i] = sums[i - 1];
+        else {
+            dp[i] = sums[i - 1] - sums[i - k - 1];
+            if (dp[i] < 0)
+                dp[i] += mod;
+        }
+        sums[i] = (sums[i - 1] + dp[i]) % mod;
+    }
 
     cout << dp[n];
 
